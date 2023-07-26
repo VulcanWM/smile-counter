b607934c91acd6ccb96d2e0b946c9213acb0bb66
VulcanWM <logmedha09@gmail.com>
2023-07-26 18:11:51 +0100

    added instructions and better css

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ function App() {
   const [smiles, setSmiles] = useState(0)
 
   return (
-    <>
-      <span>SMILES: </span><strong>{smiles} </strong>
-      <button onClick={() => setSmiles((smiles) => smiles + 1)}>
-        +
-      </button>
-      <button onClick={() => setSmiles((smiles) => smiles - 1)}>
-        -
-      </button>
-    </>
+    <div className="content">
+      <h2>Smile Counter</h2>
+      <p>Try to keep your smile count as high as possible by spreading joy.</p>
+      <p>Click <code>+</code> when you make someone smile</p>
+      <p>Click <code>-</code> when you make someone feel sad </p>
+      <span>SMILES: <strong>{smiles}</strong> </span>
+      <button onClick={() => setSmiles((smiles) => smiles + 1)}>+</button>
+      <button onClick={() => setSmiles((smiles) => smiles - 1)}>-</button>
+    </div>
   )
 }
 
